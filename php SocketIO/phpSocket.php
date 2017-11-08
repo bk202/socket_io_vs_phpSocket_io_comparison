668f629662da300f6c7349d21b0a47dec722ee8b
liujohnl <liujohnl@sfu.ca>
2017-11-08 15:06:52 -0800

    -Added phpSocket.php

--- php SocketIO/phpSocket.php
+++ php SocketIO/phpSocket.php
@@ -9,6 +9,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
 use PHPSocketIO\SocketIO;
 
+function getCurrentTime(){
+    $t = microtime(true);
+    $micro = sprintf("%06d",($t - floor($t)) * 1000000);
+    $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
+
+    return $d->format("Y-m-d H:i:s.u"); // note at point on "u"
+}
+
 $userCnt = 0;
 $maxUserCnt = 200;
 $clientCutOffEvent = 400;
@@ -17,15 +25,28 @@ $io = new SocketIO(80);
 $eventCnt = 0;
 $lastRecordedTime = microtime(true);
 $uid = 0;
-$io->on('connection', function($socket)use($io, &$userCnt, $maxUserCnt, $cutOffEvent, $lastRecordedTime, &$eventCnt, &$uid){
+$avgTimeDiff = 0;
+$io->on('connection', function($socket)use($io, &$userCnt, $maxUserCnt, $cutOffEvent, $lastRecordedTime, &$eventCnt, &$uid, &$avgTimeDiff){
     $socket->emit('generateUid', $uid++);
 
-    $socket->on('sendInfo', function($info)use($socket){
+    $socket->on('sendInfo', function($info)use($socket, &$avgTimeDiff, &$eventCnt, $maxUserCnt){
+        $eventCnt++;
         $decoded = json_decode($info, true);
         $returnString = (string)$decoded['uid'] ." : ". (string)$decoded['timeStamp'] . "\n";
         $returnResponse = ["s" => $returnString];
 
+        $serverTime = microtime(true) * 1000;
+        $clientTime = $decoded["milliseconds"];
+        $timeDiff = $serverTime - $clientTime;
+        $avgTimeDiff += $timeDiff;
+
         file_put_contents("log.txt", "s: " . $returnResponse["s"], FILE_APPEND);
+        file_put_contents("log.txt", "System time: " . getCurrentTime() . "\n", FILE_APPEND);
+        file_put_contents("log.txt", "time difference: " . $timeDiff . " milliseconds". "\n", FILE_APPEND);
+        if($eventCnt == $maxUserCnt){
+            $avgTimeDiff /= $maxUserCnt;
+            file_put_contents("log.txt", "Average time difference: $avgTimeDiff", FILE_APPEND);
+        }
         $socket->emit('returnInfo', $returnResponse);
     });
 });
